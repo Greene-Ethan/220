4ff8f49cc2f3974ff40de7379031e1adcf89701f
Greene-Ethan <greeneed@g.cofc.edu>
2022-01-20 12:28:48 -0500

    Completed hw1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,46 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Ethan Greene
+hw1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Python program that solves multiple conversion problems
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def calc_rec_area():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 
 def calc_volume():
-    pass
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height: "))
+    volume = length * width * height
+    print("volume =", volume)
 
 
 def shooting_percentage():
-    pass
+    shots = eval(input("how many shots did you shoot? "))
+    shots_made = eval(input("How many shots did you make? "))
+    percent_made = shots_made / shots * 100
+    print("You made",percent_made,"% of your shots")
 
 
 def coffee():
-    pass
+    pounds = eval(input("How many pounds of coffee would you like?"))
+    price = pounds * 10.5 + pounds * .86 + 1.50
+    print("That's",price,"$")
 
 
 def kilometers_to_miles():
-    pass
+    kilometers = eval(input("How many kilometers did you travel?"))
+    miles = kilometers / 1.61
+    print("That's",miles,"miles!")
 
 
 if __name__ == '__main__':
