5a1028b208c5487c72ba9921c52af1576d6e0a20
Greene-Ethan <greeneed@g.cofc.edu>
2022-03-23 16:58:26 -0400

    Completed lab 9

--- labs/lab9/lab9.py
+++ labs/lab9/lab9.py
@@ -1,15 +1,19 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Ethan Greene
+Lab9.py
+
+Problem: Tic Tac Toe in Python!
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
 """
 
 
 def build_board():
-    pass
-
+    return ["1","2","3","4","5","6","7","8","9"]
 
 def print_board(board):
-    """ prints the values of baord """
+    """ prints the values of board """
     RED = "\033[1;31m"
     BLUE = "\033[1;36m"
     LIGHT_GRAY = "\033[0;37m"
@@ -35,29 +39,82 @@ def print_board(board):
     print(row_3)
     print(reset)
 
-
 def is_legal(board, position):
-    pass
+    position = position -1
+    return board[position].isnumeric()
 
 
 def fill_spot(board, position, character):
-    pass
+    position = position - 1
+    character = character.strip()
+    character = character.lower()
+    if character == "x":
+        board[position] = "x"
+    else:
+        board[position] = "o"
+
 
 
 def winning_game(board):
-    pass
+    for i in range(0,8,3):
+        if board[i] == board[i + 1] == board[i+2]:
+            return True
+    for i in range(3):
+        if board[i] == board[i + 3] == board[i+6]:
+            return True
+    if board[0] == board[4]  == board[8]:
+        return True
+    if board[2] == board[4]  == board[6]:
+        return True
+    return False
+
 
 
 def game_over(board):
-    pass
-
+    if winning_game(board) == True:
+        return True
+    for i in range(9):
+        if board[i].isnumeric() == True:
+            return False
+    return True
 
 def get_winner(board):
-    pass
-
+    if board.count("x") > board.count("o"):
+        return "x"
+    elif board.count("o") == board.count("x"):
+        return "o"
+    return None
 
 def play(board):
-    pass
+    go_again = True
+    while go_again:
+        print("\n Each player takes turns putting their x's or o's in an open square."
+              " You do so by typing in the number that you would like to place your "
+              "square in. You win by getting three of your letters in a row."
+              " Good luck!")
+        turn = 1
+        while not game_over(board):
+            print_board(board)
+            if turn % 2 == 0:
+                position = eval(input("o's choose a position!"))
+                character = "o"
+            else:
+                position = eval(input("x's choose a position!"))
+                character = "x"
+            turn = turn + 1
+            while is_legal(board, position) == False:
+                position = eval(input("Sorry that didn't work, please enter a spot where no one is"))
+            fill_spot(board, position, character)
+        if winning_game(board):
+            print(get_winner(board),"won the game!! great job!")
+        else:
+            print("Uh oh it's tie!")
+        play_again = input("Would you like to go again?")
+        if play_again[0] == "y" or play_again[0] == "Y":
+            go_again = True
+        else:
+            go_again = False
+        board = build_board()
 
 
 def main():
